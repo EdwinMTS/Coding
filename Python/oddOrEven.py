6793daf3d2740bcc531885a30506457f3dbf23e0
EdwinMTS <edwinolofmartens@gmail.com>
2021-11-02 23:11:57 +0100

    OddOrEven

---
+++ Python/oddOrEven.py
@@ -0,0 +1,6 @@
+input_num = int(input('Please provide me with a number so I can check if it is odd or even: '))
+
+if(input_num % 2 == 0):
+    print(f"The number {input_num} is an even number")
+else:
+    print(f"The number {input_num} is an odd number")
